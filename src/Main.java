26a0fe469cf1ba45391f7b2148f52246629ce37b
hbala3 <hafsahbala13@gmail.com>
2025-11-17 19:26:51 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+/**
+ * The Main class serves as the entry point for the application.
+ * It prints simple welcome messages to the console.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Docker & Java!");
